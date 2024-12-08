6842315b777c5f2fcdc64afe9378af6a2bd4cbdb
zhangfuxing <zhangfuxing1010@163.com>
2024-12-08 13:31:40 +0800

    add test

---
+++ src/main/java/com/zhangfuxing/tools/Test.java
@@ -0,0 +1,13 @@
+package com.zhangfuxing.tools;
+
+/**
+ * @author 张福兴
+ * @version 1.0
+ * @date 2024/12/8
+ * @email zhangfuxing1010@163.com
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
